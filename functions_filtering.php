efd55330f461bfb10d51e397397b7e9d01bb0250
jacquelinetun <jacquelinenaychi@gmail.com>
2023-11-24 15:54:52 +0630

    php functions file

---
+++ functions_filtering.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Functions</title>
+</head>
+<body>
+    <h1>Recommended Books</h1>
+
+    <?php
+        $books = [
+            [
+                'name' => 'The 5AM Club',
+                'author' => 'Robin Sharma',
+                'releaseYear'=> '2018',
+                'purchaseUrl'=> 'https://www.amazon.com/AM-Club-Morning-Elevate-Life/dp/1443456624'
+            ],
+
+            [
+                'name' => 'Becoming',
+                'author' => 'Michelle Obama',
+                'releaseYear'=> '2018',
+                'purchaseUrl'=> 'https://www.amazon.com/Becoming-Michelle-Obama/dp/1524763136'
+            ],
+
+            [
+                'name' => 'Think Straight',
+                'author' => 'Darius Foroux',
+                'releaseYear'=> '2017',
+                'purchaseUrl' =>'https://www.amazon.com/THINK-STRAIGHT-Change-Your-Thoughts-ebook/dp/B077NJWFR3'
+
+            ]
+            
+        ];
+    ?>
+    
+    <ul>
+        <?php foreach ($books as $book): ?>
+            <?php if ($book['author'] === 'Robin Sharma'): ?>
+            <li>
+                    <a href="<?= $book['purchaseUrl'];?>">
+                        <?= $book['name']; ?> (<?= $book    ['releaseYear']?>) - By <?= $book['author']?>
+                    </a>
+                </li>
+            <?php endif ; ?>
+        <?php endforeach; ?>
+    </ul>
+    
+</body>
+</html>
